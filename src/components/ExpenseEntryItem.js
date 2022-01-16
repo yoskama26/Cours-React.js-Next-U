c4295182e62e0cb519d77596e23d2a396f50ac82
achachia <achachia2003@yahoo.fr>
2022-01-16 09:08:26 +0100

    React-JSX-V1

--- src/components/ExpenseEntryItem.js
+++ src/components/ExpenseEntryItem.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 class ExpenseEntryItem extends React.Component {
+
+   nomItem = 'Mango Juice';
+   amountItem = 30.00;
+   dateDepenseItem = 2020-10-10;
+   CategoryItem  = 'Food';
+
    render() {
       return (
          <div>
            <h2>Création d'un composant de classe</h2>
-            <div><b>Item:</b> <em>Mango Juice</em></div>
-            <div><b>Amount:</b> <em>30.00</em></div>
-            <div><b>Spend Date:</b> <em>2020-10-10</em></div>
-            <div><b>Category:</b> <em>Food</em></div>
+            <div><b>Item:</b> <em>{this.nomItem}</em></div>
+            <div><b>Montant:</b> <em>{this.amountItem}</em></div>
+            <div><b>Date de dépense:</b> <em>{this.dateDepenseItem}</em></div>
+            <div><b>Category:</b> <em>{this.CategoryItem}</em></div>
          </div>
       );
    }
