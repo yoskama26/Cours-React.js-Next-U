fcc50470efd28d582ad51870e844eb8ed8905d0f
achachia <achachia2003@yahoo.fr>
2022-01-17 07:46:04 +0100

    React-JSX-V2

--- src/components/ExpenseEntryItem.js
+++ src/components/ExpenseEntryItem.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React from "react";
 
 class ExpenseEntryItem extends React.Component {
+  nomItem = "Mango Juice";
+  amountItem = 30.0;
+  dateDepenseItem = 2020 - 10 - 10;
+  CategoryItem = "Food";
 
-   nomItem = 'Mango Juice';
-   amountItem = 30.00;
-   dateDepenseItem = 2020-10-10;
-   CategoryItem  = 'Food';
+  myelement = (
+    <div>
+      <h2>Création d'un composant de classe</h2>
+      <div>
+        <b>Item:</b> <em>{this.nomItem}</em>
+      </div>
+      <div>
+        <b>Montant:</b> <em>{this.amountItem}</em>
+      </div>
+      <div>
+        <b>Date de dépense:</b> <em>{this.dateDepenseItem}</em>
+      </div>
+      <div>
+        <b>Category:</b> <em>{this.CategoryItem}</em>
+      </div>
+    </div>
+  );
 
-   render() {
-      return (
-         <div>
-           <h2>Création d'un composant de classe</h2>
-            <div><b>Item:</b> <em>{this.nomItem}</em></div>
-            <div><b>Montant:</b> <em>{this.amountItem}</em></div>
-            <div><b>Date de dépense:</b> <em>{this.dateDepenseItem}</em></div>
-            <div><b>Category:</b> <em>{this.CategoryItem}</em></div>
-         </div>
-      );
-   }
+  render() {
+    return this.myelement;
+  }
 }
 export default ExpenseEntryItem;
