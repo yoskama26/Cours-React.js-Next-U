29bff711106df56d44863be24488ff0a8c3bcf73
achachia <achachia2003@yahoo.fr>
2022-01-17 08:48:30 +0100

    React-JSX-V3

--- src/components/ExpenseEntryItem.js
+++ src/components/ExpenseEntryItem.js
@@ -1,25 +1,42 @@
 import React from "react";
 
+import "./ExpenseEntryItem.css";
+
+import imgItem from "./../assets/images/image_produit.png";
+
+const class_name = "red_h2";
+
 class ExpenseEntryItem extends React.Component {
   nomItem = "Mango Juice";
   amountItem = 30.0;
   dateDepenseItem = 2020 - 10 - 10;
   CategoryItem = "Food";
 
+  getCurrentTime() {
+    return new Date().toTimeString();
+  }
+
   myelement = (
     <div>
       <h2>Création d'un composant de classe</h2>
+      <h2 className={class_name}>
+        L'heure actuelle est {this.getCurrentTime()}
+      </h2>
       <div>
-        <b>Item:</b> <em>{this.nomItem}</em>
+        <img className="class_image_item" src={imgItem} alt="image item " />
       </div>
       <div>
-        <b>Montant:</b> <em>{this.amountItem}</em>
+        <b className="class_item">Item:</b> <em>{this.nomItem}</em>
       </div>
       <div>
-        <b>Date de dépense:</b> <em>{this.dateDepenseItem}</em>
+        <b className="class_item">Montant:</b> <em>{this.amountItem}</em>
       </div>
       <div>
-        <b>Category:</b> <em>{this.CategoryItem}</em>
+        <b className="class_item">Date de dépense:</b>{" "}
+        <em>{this.dateDepenseItem}</em>
+      </div>
+      <div>
+        <b className="class_item">Categorie:</b> <em>{this.CategoryItem}</em>
       </div>
     </div>
   );
